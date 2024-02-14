226dcf2fe7765b8a0d00bee2bcc1b74220259b8f
jmkivett93 <jordan@jgtitleco.com>
2024-02-13 22:46:42 -0600

    second commit

--- Variables/obj/Debug/net8.0/Variables.AssemblyInfo.cs
+++ Variables/obj/Debug/net8.0/Variables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2ad271623312dc11590500d510875d6ed3c63ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41b93c7a79221b9d80ae7b8372523a6cdd259d8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
